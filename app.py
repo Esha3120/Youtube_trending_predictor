cc19c243b644a09c6a10ceb1f7f7d4caff744e51
Esha3120 <eshabodhani01@gmail.com>
2025-05-26 08:55:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from datetime import datetime, time
 import pandas as pd
-!pip install joblib
+pip install joblib
 import joblib
 from utils.data_processing import prepare_data, enhance_features
 from googleapiclient.discovery import build
